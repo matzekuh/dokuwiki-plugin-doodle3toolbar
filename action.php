c22883522d14bffc498732ff321a13c74d5dd6c1
matzekuh <matthias.jung@fmc.uni-karlsruhe.de>
2016-02-01 22:25:08 +0100

    Update to newest dev

--- action.php
+++ action.php
@@ -1,28 +1,22 @@
 <?php
-require_once(DOKU_PLUGIN.'action.php');
+if (!defined('DOKU_INC')) die();
 
-class doodle3toolbar extends DokuWiki_Action_Plugin {
-	
-	/**
-	 * Register its handlers with the dokuwiki's event controller
-	 */
-	function register(Doku_Event_Handler $controller) {
-		$controller->register_hook('TOOLBAR_DEFINE', 'AFTER', $this, 'insert_button', array());
-	}
-	
-	function insert_button(& $event, $param) {
-		$template = file_get_contents('template.txt')
-		
-		if $template==false return;
-		
-		$event->data[] = array (
-		'type' => 'insert',
-		'title' => 'doodle3',
-		'icon' => '../../plugins/doodle3toolbar/icon.png',
-		'insert' => $this->getConf('toolbar_inserted_markup')
-		);
-	}
+class action_plugin_doodle3toolbar extends DokuWiki_Action_Plugin {
+
+        /**
+         * Register its handlers with the dokuwiki's event controller
+         */
+        public function register(Doku_Event_Handler $controller) {
+                $controller->register_hook('TOOLBAR_DEFINE', 'AFTER', $this, 'insert_button', array());
+        }
+
+        public function insert_button(Doku_Event $event, $param) {
+                $event->data[] = array (
+                'type' => 'insert',
+                'title' => 'doodle3',
+                'icon' => '../../plugins/doodle3toolbar/icon.png',
+                'insert' => $this->getConf('inserttext')
+                );
+        }
 }
-
-
 ?>
---
+++ conf/default.php
@@ -0,0 +1,3 @@
+<?php
+$conf['inserttext'] = '<doodle\n  title="TITEL AENDERN"\n  auth="user"\n  adminUsers="testuser"\n  adminGroups=""\n  voteType="multi"\n  closed="false"\n>\n  * Option1\n  * Option2\n</doodle>';
+?>
