3cd1325a6eb15575e52f0e6c6764fa842e3f2139
matzekuh <matzekuh@users.noreply.github.com>
2016-02-01 22:27:40 +0100

    Update action.php

--- action.php
+++ action.php
@@ -1,30 +1,7 @@
 <?php
 if (!defined('DOKU_INC')) die();
 
-<<<<<<< HEAD
 class action_plugin_doodle3toolbar extends DokuWiki_Action_Plugin {
-=======
-class doodle3toolbar extends DokuWiki_Action_Plugin {
-	
-	/**
-	 * Register its handlers with the dokuwiki's event controller
-	 */
-	function register(Doku_Event_Handler $controller) {
-		$controller->register_hook('TOOLBAR_DEFINE', 'AFTER', $this, 'insert_button', array());
-	}
-	
-	function insert_button(& $event, $param) {
-		$template = file_get_contents('template.txt');
-		
-		$event->data[] = array (
-		'type' => 'insert',
-		'title' => 'doodle3',
-		'icon' => '../../plugins/doodle3toolbar/icon.png',
-		'insert' => $this->getConf('toolbar_inserted_markup')
-		);
-	}
-}
->>>>>>> origin/master
 
         /**
          * Register its handlers with the dokuwiki's event controller
@@ -33,7 +10,6 @@ class doodle3toolbar extends DokuWiki_Action_Plugin {
                 $controller->register_hook('TOOLBAR_DEFINE', 'AFTER', $this, 'insert_button', array());
         }
 
-<<<<<<< HEAD
         public function insert_button(Doku_Event $event, $param) {
                 $event->data[] = array (
                 'type' => 'insert',
@@ -43,6 +19,4 @@ class doodle3toolbar extends DokuWiki_Action_Plugin {
                 );
         }
 }
-=======
->>>>>>> origin/master
 ?>
