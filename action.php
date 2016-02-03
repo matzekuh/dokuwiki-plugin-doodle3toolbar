054a95c7b2d95c2c27f067eaa5a42127201e5b61
matzekuh <matzekuh@users.noreply.github.com>
2016-02-03 14:41:31 +0100

    Update action.php

--- action.php
+++ action.php
@@ -1,4 +1,13 @@
 <?php
+
+/**
+ * Doodle Plugin 3: helps to schedule meetings
+ *
+ * @license	GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @url         http://www.dokuwiki.org/plugin:doodle3toolbar
+ * @author	Matthias Jung <matzekuh@web.de>
+ */
+
 if (!defined('DOKU_INC')) die();
 
 class action_plugin_doodle3toolbar extends DokuWiki_Action_Plugin {
@@ -9,7 +18,10 @@ class action_plugin_doodle3toolbar extends DokuWiki_Action_Plugin {
         public function register(&$controller) {
                 $controller->register_hook('TOOLBAR_DEFINE', 'AFTER', $this, 'insert_button', array());
         }
-
+        
+        /**
+         * Define a toolbar button, that inserts a text defined in the addon conf
+         */
         public function insert_button(&$event, $param) {
                 $event->data[] = array (
                 'type' => 'insert',
