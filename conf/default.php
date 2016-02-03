45c39d5297934f8a12b35dfa0d8e3847fe615d6a
matzekuh <matzekuh@users.noreply.github.com>
2016-02-03 15:38:16 +0100

    Update default.php

--- conf/default.php
+++ conf/default.php
@@ -1,7 +1,7 @@
 <?php
-$conf['default_auth'] = 'user';
-$conf['default_admin_groups'] = '';
-$conf['default_vote_type'] = 'multi';
+$conf['doodle3toolbar_default_auth'] = 'user';
+$conf['doodle3toolbar_default_admin_groups'] = '';
+$conf['doodle3toolbar_default_vote_type'] = 'multi';
 
 //DEL wenn fertig
 $conf['inserttext'] = '<doodle\n  title="TITEL AENDERN"\n  auth="user"\n  adminUsers="testuser"\n  adminGroups=""\n  voteType="multi"\n  closed="false"\n>\n  * Option1\n  * Option2\n</doodle>';
